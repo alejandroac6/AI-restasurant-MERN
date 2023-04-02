11bacde6a60a4ca658e7658e2749eddb87ff2afd
Alejandro Alarcon <alejandro.alarcon.cano@gmail.com>
2023-04-02 22:16:20 +0200

    Backend: añadiendo variables de entorno

--- backend/conifg/db.js
+++ backend/conifg/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const conectarDB = async ()=>{
     try {
-        const connection = await mongoose.connect("mongodb+srv://Alejandro:root@cluster0.7nephvf.mongodb.net/Autonomous?retryWrites=true&w=majority",{
+        
+        const connection = await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser: true,
             useUnifiedTopology:true,
         })
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 import express from "express"
+import dotenv from 'dotenv'
 import conectarDB from "./conifg/db.js"
+
 const app=express()
+
+dotenv.config()
 
 conectarDB()
 
+const PORT = process.env.PORT
+
 console.log('estoy haciendo pruebas')
-app.listen(4000,()=>{
+app.listen(PORT,()=>{
     console.log('Estoy en el puerto 4000')
 })
