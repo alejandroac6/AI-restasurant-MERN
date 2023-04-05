568d39554de72e2a2f029f8258129a952568eb00
Alejandro <alejandro.alarcon.cano@gmail.com>
2023-04-05 17:18:09 +0200

    Backend: hasheando passwords

--- backend/models/Usuario.js
+++ backend/models/Usuario.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
 
 const usuarioSchema=mongoose.Schema({
     nombre:{
@@ -30,6 +31,20 @@ const usuarioSchema=mongoose.Schema({
 {
     // para crear 2 columnas mas, la de tiempo de creacion y la del tiempo de actualizacion
     timestamps:true,
+})
+
+usuarioSchema.pre('save',async function(next){
+    // cuando estemos editando el perfil, en caso de que no se este modificando el password, que no haga nada, que vaya al siguiente middleware, por eso suamos next
+
+    if(!this.isModified('password')){
+        next()
+    }
+    // hacemos operaciones en el servidor y lo aplicara en el salt, tema hasheo por rondas
+    const salt = await bcrypt.genSalt(10);
+
+    // utilizamos function() porque necesitaremos acceder a this.
+    this.password= await bcrypt.hash(this.password,salt);
+
 })
 
 const Usuario=mongoose.model("Usuario",usuarioSchema);
